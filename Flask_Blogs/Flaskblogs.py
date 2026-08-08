8311ae184af59d127ec338a127d69a102860708a
sumitc0144 <sumitc0144@gmail.com>
2026-08-08 11:44:02 +0530

    Flask_Blog1

--- Flask_Blogs/Flaskblogs.py
+++ Flask_Blogs/Flaskblogs.py
@@ -1,14 +1,31 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 app=Flask(__name__)
+
+
+posts=[
+    {
+        'author':'John Doe',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'April 20,2024'
+    },
+    {
+        'author':'Jane Smith',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'April 21,2024'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title="ABOUT"
+    "")
 
 if __name__=="__main__":
     app.run(debug=True)
