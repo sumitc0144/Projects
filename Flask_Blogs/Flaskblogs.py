9c3e490e849c83106df43d3f0f7b82c627c60425
sumitc0144 <sumitc0144@gmail.com>
2026-08-08 09:55:09 +0530

    Flask_Blog1

--- Flask_Blogs/Flaskblogs.py
+++ Flask_Blogs/Flaskblogs.py
@@ -2,5 +2,13 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1>HOME PAGE</h1>"
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
