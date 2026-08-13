d4a99365b77a3afa0a0c7b5f270136b7ca059097
sumitc0144 <sumitc0144@gmail.com>
2026-08-13 13:00:32 +0530

    Flask_blog_Profile Info and Profile picture

--- Flask_Blogs/flaskblog/routes.py
+++ Flask_Blogs/flaskblog/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, flash, redirect,request
-from flaskblog.Forms import RegistrationForm, LoginForm,UpdateAccountForm
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog.Forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required   
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 posts = [
@@ -36,7 +39,7 @@ def about():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for("home"))
-    
+
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
@@ -61,10 +64,10 @@ def login():
             login_user(user, remember=form.remember.data)
             flash("You have been logged in!", "success")
             next_page = request.args.get("next")
-            
+
             return redirect(next_page) if next_page else redirect(url_for("home"))
-            
-        else:             
+
+        else:
             flash("Login Unsuccessful. Please check email and password", "danger")
         # return f'Login successful for {form.email.data}!'
     return render_template("login.html", title="Login", form=form)
@@ -75,18 +78,44 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/account",methods=["GET", "POST"])
-@login_required 
+
+# Generate a secure random 16-character hexadecimal string
+# (using 8 random bytes) to create a unique filename for the uploaded picture.
+# This prevents filename collisions and improves security.
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+
+    # Resize the image to a maximum of 125x125 pixels while maintaining the aspect ratio.
+    output_size = (125, 125)
+    i=Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    #form_picture.save(picture_path)
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.route("/account", methods=["GET", "POST"])
+@login_required
 def account():
-    form=UpdateAccountForm()
+    form = UpdateAccountForm()
     if form.validate_on_submit():
-        current_user.username=form.username.data
-        current_user.email=form.email.data
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
         db.session.commit()
         flash("Your account has been updated!", "success")
-        return redirect(url_for('account'))
-    elif request.method =='GET':
-        form.username.data=current_user.username
-        form.email.data=current_user.email
-    image_file=url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template("account.html", title="Account", image_file=image_file,form=form)
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
