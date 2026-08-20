d9c740d2513e32e8714e0fb47e99634997c8aca6
sumitc0144 <sumitc0144@gmail.com>
2026-08-20 20:42:38 +0530

    Flaskblogs Set/Reset

--- Flask_Blogs/flaskblog/Forms.py
+++ Flask_Blogs/flaskblog/Forms.py
@@ -52,3 +52,4 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
