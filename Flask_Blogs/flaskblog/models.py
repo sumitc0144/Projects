a36dba79098f00cd1dba3e3ba40034938413ddd7
sumitc0144 <sumitc0144@gmail.com>
2026-08-20 19:59:27 +0530

    Flaskblogs Set/Reset

--- Flask_Blogs/flaskblog/models.py
+++ Flask_Blogs/flaskblog/models.py
@@ -1,4 +1,5 @@
-from flaskblog import db,login_manager
+from flaskblog import db,login_manager,app
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask_login import UserMixin
 from datetime import datetime
 
@@ -15,6 +16,20 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship("Post", backref="author", lazy=True)
+
+    def get_reset_token(self):
+        s = Serializer(app.config["SECRET_KEY"])
+        return s.dumps({"user_id": self.id})
+
+    @staticmethod
+    def verify_reset_token(token, expiration=1800):
+        s = Serializer(app.config["SECRET_KEY"])
+        try:
+            user_id = s.loads(token, max_age=expiration)["user_id"]
+        except:
+            return None
+        return User.query.get(user_id)
+
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
