387e14546c84e4a27e8e352a7ffd6b1666d51681
sumitc0144 <sumitc0144@gmail.com>
2026-08-24 23:08:59 +0530

    Flaskblogs Set/Reset send email

--- Flask_Blogs/flaskblog/routes.py
+++ Flask_Blogs/flaskblog/routes.py
@@ -204,6 +204,7 @@ def send_reset_email(user):
 
 If you did not make this request then simply ignore this email and no changes will be made.
 """
+    mail.send(msg)
 
 
 @app.route("/reset_password", methods=["GET", "POST"])
