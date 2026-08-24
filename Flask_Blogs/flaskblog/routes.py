cf6ce0c179b349263291ed399b3ef53e9a9cb603
sumitc0144 <sumitc0144@gmail.com>
2026-08-24 23:19:36 +0530

    Flaskblogs Set/Reset send email changes in routes.py

--- Flask_Blogs/flaskblog/routes.py
+++ Flask_Blogs/flaskblog/routes.py
@@ -196,7 +196,7 @@ def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message(
         "Password Reset Request",
-        sender="noreply@blog.com",
+        sender=os.environ.get("EMAIL_USER"),
         recipients=[user.email]
     )
     msg.body = f"""To reset your password, visit the following link:
