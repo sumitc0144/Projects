fc2cbde1a0130a4e3be672ed58c6ac6c963c09d0
sumitc0144 <sumitc0144@gmail.com>
2026-08-24 22:51:24 +0530

    Flaskblogs Set/Reset send email

--- Flask_Blogs/flaskblog/__init__.py
+++ Flask_Blogs/flaskblog/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt  # Password Hashing with Flask-Bcrypt
@@ -15,8 +16,8 @@ login_manager.login_message_category = "info"
 app.config["MAIL_SERVER"] = "smtp.googlemail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "your-email@gmail.com"
-app.config["MAIL_PASSWORD"] = "your-password"
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASSWORD")
 
 mail = Mail(app)
 from flaskblog import routes
