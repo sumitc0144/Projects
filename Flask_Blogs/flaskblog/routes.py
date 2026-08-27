832189219bfa77ef5cfeda15e35709602e3145d0
sumitc0144 <sumitc0144@gmail.com>
2026-08-27 20:02:34 +0530

    Blueprint

--- Flask_Blogs/flaskblog/routes.py
+++ Flask_Blogs/flaskblog/routes.py
@@ -29,27 +29,6 @@ def about():
     return render_template("about.html", title="About")
 
 
-@app.route("/account", methods=["GET", "POST"])
-@login_required
-def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash("Your account has been updated!", "success")
-        return redirect(url_for("account"))
-    elif request.method == "GET":
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
-    return render_template(
-        "account.html", title="Account", image_file=image_file, form=form
-    )
-
 
 @app.route("/post/new", methods=["GET", "POST"])
 @login_required
@@ -107,64 +86,4 @@ def delete_post(post_id):
     return redirect(url_for("home"))
 
 
-@app.route("/user/<string:username>")
-def user_posts(username):
-    page = request.args.get("page", 1, type=int)
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = (
-        Post.query.filter_by(author=user)
-        .order_by(Post.date_posted.desc())
-        .paginate(page=page, per_page=5)
-    )
-    return render_template("user_posts.html", posts=posts, user=user)
 
-
-def send_reset_email(user):
-    token = user.get_reset_token()
-    msg = Message(
-        "Password Reset Request",
-        sender=os.environ.get("EMAIL_USER"),
-        recipients=[user.email],
-    )
-
-    msg.body = f"""To reset your password, visit the following link:
-{url_for("reset_token", token=token, _external=True)}
-
-If you did not make this request then simply ignore this email and no changes will be made.
-"""
-    mail.send(msg)
-
-
-@app.route("/reset_password", methods=["GET", "POST"])
-def reset_request():
-    if current_user.is_authenticated:
-        return redirect(url_for("home"))
-    form = RequestResetForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        send_reset_email(user)
-        flash(
-            "An email has been sent with instructions to reset your password.", "info"
-        )
-        return redirect(url_for("login"))
-    return render_template("reset_request.html", title="Reset Password", form=form)
-
-
-@app.route("/reset_password/<token>", methods=["GET", "POST"])
-def reset_token(token):
-    if current_user.is_authenticated:
-        return redirect(url_for("home"))
-    user = User.verify_reset_token(token)
-    if user is None:
-        flash("That is an invalid or expired token", "warning")
-        return redirect(url_for("reset_request"))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
-            "utf-8"
-        )
-        user.password = hashed_password
-        db.session.commit()
-        flash("Your password has been updated! You are now able to log in", "success")
-        return redirect(url_for("login"))
-    return render_template("reset_token.html", title="Reset Password", form=form)
--- Flask_Blogs/flaskblog/users/routes.py
+++ Flask_Blogs/flaskblog/users/routes.py
@@ -3,7 +3,8 @@ from flask import Blueprint
 users = Blueprint('users', __name__)
 
 
-@app.route("/register", methods=["GET", "POST"])
+# @app.route("/register", methods=["GET", "POST"])
+@users.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for("home"))
@@ -22,7 +23,8 @@ def register():
     return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login", methods=["GET", "POST"])
+# @app.route("/login", methods=["GET", "POST"])
+@users.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("home"))
@@ -38,8 +40,83 @@ def login():
     return render_template("login.html", title="Login", form=form)
 
 
-@app.route("/logout")
+# @app.route("/logout")
+@users.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+
+# @app.route("/account", methods=["GET", "POST"])
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
+
+
+
+# @app.route("/user/<string:username>")
+@users.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = (
+        Post.query.filter_by(author=user)
+        .order_by(Post.date_posted.desc())
+        .paginate(page=page, per_page=5)
+    )
+    return render_template("user_posts.html", posts=posts, user=user)
+
+
+
+# @app.route("/reset_password", methods=["GET", "POST"])
+@users.route("/reset_password", methods=["GET", "POST"])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash(
+            "An email has been sent with instructions to reset your password.", "info"
+        )
+        return redirect(url_for("login"))
+    return render_template("reset_request.html", title="Reset Password", form=form)
+
+
+@app.route("/reset_password/<token>", methods=["GET", "POST"])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is an invalid or expired token", "warning")
+        return redirect(url_for("reset_request"))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user.password = hashed_password
+        db.session.commit()
+        flash("Your password has been updated! You are now able to log in", "success")
+        return redirect(url_for("login"))
+    return render_template("reset_token.html", title="Reset Password", form=form)
 
