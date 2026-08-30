74448e862255744edb822a25002f23d8b4269200
sumitc0144 <sumitc0144@gmail.com>
2026-08-30 23:06:06 +0530

    Custom Error handlers.py

--- Flask_Blogs/errors/handlers.py
+++ Flask_Blogs/errors/handlers.py
@@ -1,15 +1,15 @@
-# Import Blueprint from Flask to create modular components
 from flask import Blueprint, render_template
 
-# Create a Blueprint named 'errors' for handling error pages
-# '__name__' tells Flask where this Blueprint is defined
-errors = Blueprint('errors', __name__)
+
+errors = Blueprint("errors", __name__)
+
 
 # Register a custom handler for 404 errors (Page Not Found)
 @errors.app_errorhandler(404)
 def error_404(error):
     # Render a custom 404 error template and return HTTP status code 404
     return render_template("errors/404.html", title="Page Not Found"), 404
+
 
 # Register a custom handler for 500 errors (Internal Server Error)
 @errors.app_errorhandler(500)
