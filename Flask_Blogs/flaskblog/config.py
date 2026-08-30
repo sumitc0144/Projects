8ac35ccf0053fa1e0a2b0cd3c3b0d21b869ec8be
sumitc0144 <sumitc0144@gmail.com>
2026-08-30 16:27:33 +0530

    Confing Blueprint

--- Flask_Blogs/flaskblog/config.py
+++ Flask_Blogs/flaskblog/config.py
@@ -1,4 +1,8 @@
-
-
 class Config:
-    pass
+    SECRET_KEY = "5791628bb0b13ce0c676dfde280ba245"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    MAIL_SERVER= "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("EMAIL_USER")
+    MAIL_PASSWORD = os.environ.get("EMAIL_PASS")
