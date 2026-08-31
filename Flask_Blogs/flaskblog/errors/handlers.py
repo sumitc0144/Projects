cfde129415cab357c98329acea821a4b9569d09f
sumitc0144 <sumitc0144@gmail.com>
2026-08-31 09:04:52 +0530

    Custom Error handlers.py

--- Flask_Blogs/flaskblog/errors/handlers.py
+++ Flask_Blogs/flaskblog/errors/handlers.py
@@ -10,6 +10,12 @@ def error_404(error):
     # Render a custom 404 error template and return HTTP status code 404
     return render_template("errors/404.html", title="Page Not Found"), 404
 
+# Handle 403 Forbidden errors (user not allowed to access a resource)
+@errors.app_errorhandler(403)
+def error_403(error):
+    # Render a custom 403 error template and return HTTP status code 403
+    return render_template("errors/403.html", title="Forbidden Access"), 403
+
 
 # Register a custom handler for 500 errors (Internal Server Error)
 @errors.app_errorhandler(500)
